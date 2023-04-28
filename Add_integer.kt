f32bea542fc500b4a9f80f2d3358e2466d817c82
Jical <120706352+Jical@users.noreply.github.com>
2023-04-28 10:49:44 +0530

    Add files via upload

---
+++ Add_integer.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main(){
+
+    var scanner = Scanner(System.`in`)
+
+    print("Enter num 1: ")
+    var num1 = scanner.nextInt()
+
+    print("Enter num 2: ")
+    var num2 = scanner.nextInt()
+
+    println("Addition is: ${num1+num2}")
+
+
+}
